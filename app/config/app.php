56c11af5d1bad32937a11a917dccb8197552163d
Chung Tran <huychungtran@gmail.com>
2014-06-14 15:44:45 +0900

    add Sentry service

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
 		'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
 		'Barryvdh\Debugbar\ServiceProvider',
 
 	),
@@ -191,6 +192,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Sentry' 		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
