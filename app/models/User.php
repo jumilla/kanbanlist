79245a0ff30a29c68cb11eed61085901665b199a
Tsuyoshi Seino <seino.tsuyoshi@classmethod.jp>
2014-06-14 14:24:22 +0900

    delete and update function

--- app/models/User.php
+++ app/models/User.php
@@ -4,8 +4,6 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
@@ -23,84 +21,54 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
-	
-	
-	private $_email;
-	private $_password;
-	private $_password_confirmation;
-	private $_remember_me;
-	private $_name;
-	private $_bg_img;
-	private $_layout;
-	private $_pomo;
-	
+	//protected $hidden = array('password', 'remember_token');
+		
 	private $_base_bg_path;
 	
 	public function bgImgPath(){
-		
+		if ($this->bg_img){
+			return $this->bg_img;
+		}else{
+			return null;
+		}
 	}
-	
-	public function addUser($name){
 		
-	}
-	
-	public function byName($name){
-		return User::where('name', '=', $name)->firstOrFail();
-		App::error(function(ModelNotFoundException $e)
-		{
-		    return null;
-		});
+	public static function byName($name){
+		return User::where('name', '=', $name)->first();
+		
 	}
 	
 	public function exist($name){
-		if (User::where('name', '=', $name)->get()){
+		$user = self::byName($name);
+		if ($user){
 			return true;
 		}
 		return false;
 	}
 	
 	public function bgImgByName($name){
-		$user = $this->byName($name);
+		$user = self::byName($name);
 		if ($user){
 			return $user->bg_img;
 		}
 		return null;
 	}
 	
-	public function setBgImg($name, $bg_img){
-		$user = $this->byName($name);
-		if (!$user){
-			return null;
-		}
-		$user->bg_img = $bg_img;
-		$user->save();
-	}
 	
 	public function layoutByName($name){
-		$user = $this->byName($name);
+		$user = self::byName($name);
 		if ($user){
 			return $user->layout;
 		}
 		return null;
 	}
 	
-	public function setLayout($name, $layout){
-		$user = $this->byName($name);
-		if (!$user){
-			return null;
-		}
-		$user->layout = $layout;
-		$user->save();
-	}
-	
 	public function incPomo($name){
-		$user = $this->byName($name);
+		$user = self::byName($name);
 		if (!$user){
 			return null;
 		}
 		$user->pomo += 1;
 		$user->save();
 	}
-
 }
