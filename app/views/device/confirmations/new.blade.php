4265ccf2844edca180c2ae3bc19b6c67843dee0e
yutayokoi <yuta.yokoi.recruit@gmail.com>
2014-06-21 15:37:17 +0900

    confirmations,mailer,passwords tpl edit

--- app/views/device/confirmations/new.blade.php
+++ app/views/device/confirmations/new.blade.php
@@ -3,8 +3,8 @@
 {{ Form::open(['url' => 'confirmation_path($resource_name)', 'method' => 'post') }}
   {{ $device_error_messages }}
 
-  <p>{{ Form::label('email', 'email',) }}<br />
-  {{ Form::email('email') }}</p>
+  <p>{{ Form::label('email', 'email') }}<br />
+  {{ Form::email('email','email') }}</p>
 
   <p>{{ Form::submit('Resend confirmation instructions') }}</p>
 {{ Form::close() }}
