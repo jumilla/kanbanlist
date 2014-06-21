4bce42891e9a69c72b2ee6ce3cf7e09c7fc6d88d
Yuta Ohashi <ooohhhaaassshhhiii@gmail.com>
2014-06-21 15:19:44 +0900

    add Vagrantfile and edit vagrant setting.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*.local'),
+	'local' => array('*.local','*.vagrantup.com'),
 	'staging' => array('*.hmk-temp.com'),
 
 ));
