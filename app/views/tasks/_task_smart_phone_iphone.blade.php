f31ca46c9d69fc773a71d573bdb2e1052b8b7609
geduld <desktopcorps@gmail.com>
2014-06-21 12:39:11 +0900

    fixed errors

--- app/views/tasks/_task_smart_phone_iphone.blade.php
+++ app/views/tasks/_task_smart_phone_iphone.blade.php
@@ -1,9 +1,9 @@
-<li id="id_{{ $task.id }}">
-  <a href="#setting?id={{ $task.id }}" id="edit_task_link_{{ $task.id }}" data-transition="slidefade">
+<li id="id_{{ $task->id }}">
+  <a href="#setting?id={{ $task->id }}" id="edit_task_link_{{ $task->id }}" data-transition="slidefade">
     <p>
     <table style="width: 100%;"><tr>
-        <td style="width: 40px"><span class="label label-info" id="updated_{{ $task.id }}" style="display:inline;">{{ $task.updated_at.strftime("%m/%d") }}</span></td>
-        <td class="task-body"><span id="msg_{{ $task.id }}" style="word-break:break-all;"></span></td>
+        <td style="width: 40px"><span class="label label-info" id="updated_{{ $task->id }}" style="display:inline;">{{ $task->updated_at.strftime("%m/%d") }}</span></td>
+        <td class="task-body"><span id="msg_{{ $task->id }}" style="word-break:break-all;"></span></td>
     </tr></table>
     </p>
   </a>
@@ -13,6 +13,6 @@
 (function(){
   var taskAction = KanbanList.taskAction;
   var msg_array = {{ to_js_array(task.msg) }};
-  taskAction.initial({{ $task.id }}, "{{ $task.status_sym }}", msg_array);
+  taskAction.initial({{ $task->id }}, "{{ $task->status_sym }}", msg_array);
 }());
 </script>
