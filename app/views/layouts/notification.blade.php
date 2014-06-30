a11d5c2129b4b3e3f4477143c668b4fbfdd28b47
Fumio Furukawa <fumio@straight-spirits.com>
2014-06-30 09:51:00 +0900

    View変数の$errorsが存在しない場合がある状況に対応。 Laravel4.2.6にアップデートしてから出るようになった？

--- app/views/layouts/notification.blade.php
+++ app/views/layouts/notification.blade.php
@@ -6,7 +6,7 @@
 </div>
 @endif
 
-@if ($errors->any())
+@if (isset($errors) && $errors->any())
 <div class="alert alert-danger fade in">
   <button type="button" class="close" data-dismiss="alert"></button>
 @foreach ($errors->all() as $error)
